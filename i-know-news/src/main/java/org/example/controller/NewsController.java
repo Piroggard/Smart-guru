cfdf463a0bf502e6724e5baeb122f45566781907
Piroggard <piroggard@yandex.ru>
2024-02-18 17:49:27 +0300

    Добавление модуля новостей

---
+++ i-know-news/src/main/java/org/example/controller/NewsController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/news")
+@Slf4j
+@AllArgsConstructor
+@RestController
+public class NewsController {
+
+}
---
+++ i-know-news/src/main/java/org/example/model/News.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+public class News {
+    Long id;
+    String title;
+    Long category;
+    String photo;
+    String description;
+    Long courseId;
+
+}
