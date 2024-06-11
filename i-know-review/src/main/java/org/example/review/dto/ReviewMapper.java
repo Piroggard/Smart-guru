db997e2aff0de16fc6cfe47a3febea8388fcbab9
Olesia <olesia04@yandex.ru>
2024-06-11 16:47:51 +0400

    1) Удален слой ReviewStorage, логика перенесена в сервис

--- i-know-review/src/main/java/org/example/review/dto/ReviewMapper.java
+++ i-know-review/src/main/java/org/example/review/dto/ReviewMapper.java
@@ -2,13 +2,9 @@ package org.example.review.dto;
 
 import org.example.review.model.Review;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
-
-    ReviewMapper INSTANCE = Mappers.getMapper(ReviewMapper.class);
-
     ReviewDto toDto(Review review);
     Review toEntity(ReviewDto reviewDto);
 
--- i-know-review/src/main/java/org/example/review/service/ReviewServiceImpl.java
+++ i-know-review/src/main/java/org/example/review/service/ReviewServiceImpl.java
@@ -2,52 +2,61 @@ package org.example.review.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.review.db.ReviewStorage;
+import org.example.review.db.JpaReviewRepository;
 import org.example.review.dto.ReviewDto;
+import org.example.review.dto.ReviewMapper;
 import org.example.review.exception.BadRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.example.review.model.Review;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ReviewServiceImpl implements ReviewServiceInterface {
-    private final ReviewStorage reviewStorage;
+    private final JpaReviewRepository jpaReviewRepository;
+    private final ReviewMapper reviewMapper;
+
 
 
     @Transactional
     public ReviewDto getReviewById(Long reviewId) {
-        return reviewStorage.getReviewById(reviewId);
+        return reviewMapper.toDto(jpaReviewRepository.getReviewById(reviewId));
     }
 
     @Transactional
     public ReviewDto addReview(ReviewDto reviewDto) {
-        return reviewStorage.addReview(reviewDto);
+        Review review = reviewMapper.toEntity(reviewDto);
+        return reviewMapper.toDto(jpaReviewRepository.save(review));
     }
 
     @Transactional
     public ReviewDto updateReview(ReviewDto reviewDto, Long reviewId) {
-        if(reviewStorage.getReviewById(reviewId).equals(null)) {
+        if(jpaReviewRepository.getReviewById(reviewId).equals(null)) {
             throw new BadRequestException(HttpStatus.BAD_REQUEST, "Такого отзыва не существует" + reviewId);
         }
-        return reviewStorage.updateReview(reviewDto);
+        Review review = reviewMapper.toEntity(reviewDto);
+        return reviewMapper.toDto(jpaReviewRepository.save(review));
     }
 
     @Transactional
     public void deleteReview(Long reviewId) {
-        if(reviewStorage.getReviewById(reviewId).equals(null)) {
+        if(jpaReviewRepository.getReviewById(reviewId).equals(null)) {
             throw new BadRequestException(HttpStatus.BAD_REQUEST, "Такого отзыва не существует" + reviewId);
         } else {
-            reviewStorage.deleteReview(reviewId);
+            jpaReviewRepository.deleteReviewById(reviewId);
         }
     }
 
     @Transactional
     public List<ReviewDto> getAllReviewsByUserId(Long userId) {
-        return reviewStorage.getAllReviewsByUserId(userId);
+        List<Review> reviewList = jpaReviewRepository.getAllReviewsByUserId(userId);
+        return reviewList.stream()
+                .map(reviewMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
