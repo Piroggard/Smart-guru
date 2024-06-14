99cc5bdfc5bf50d811cc93f44a5a8d9d48f62fba
kxnvg <nikita.armeec.88@mail.ru>
2024-06-14 19:06:53 +0300

    bugfix

---
+++ notification-service/src/main/java/notifications/NotificationServiceRunner.java
@@ -0,0 +1,12 @@
+package notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceRunner.class, args);
+    }
+}
