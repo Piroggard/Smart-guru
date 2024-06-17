56ff918ffd4237ad5f02cd7a4392c87ce7492374
Olesia <olesia04@yandex.ru>
2024-06-17 17:12:14 +0400

    Привела review модуль в соответствие с требованиями к KAN-24

--- review-service/src/main/java/org/example/review/dto/ReviewDto.java
+++ review-service/src/main/java/org/example/review/dto/ReviewDto.java
@@ -1,7 +1,5 @@
 package org.example.review.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,19 +12,10 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class ReviewDto {
-
-    @NotNull(message = "Идентификатор курса не должен быть пустым")
     private Long id;
-
-    @NotBlank(message = "Название не должно быть пустым")
     private String title;
-
-    @NotBlank(message = "Описание не должно быть пустым")
     private String description;
-
-    @NotBlank(message = "Время не должно быть пустым")
     private LocalDateTime postDate;
-
-    @NotNull(message = "Идентификатор пользователя не должен быть пустым")
+    private Long courseId;
     private Long userId;
 }
--- review-service/src/main/java/org/example/review/service/ReviewServiceImpl.java
+++ review-service/src/main/java/org/example/review/service/ReviewServiceImpl.java
@@ -2,45 +2,68 @@ package org.example.review.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.review.db.ReviewStorage;
+import org.example.review.db.JpaReviewRepository;
 import org.example.review.dto.ReviewDto;
+import org.example.review.dto.ReviewMapper;
 import org.example.review.exception.BadRequestException;
+import org.example.review.model.Review;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ReviewServiceImpl implements ReviewServiceInterface {
-    private final ReviewStorage reviewStorage;
+    private final JpaReviewRepository jpaReviewRepository;
+    private final ReviewMapper reviewMapper;
 
 
+
+    @Transactional
     public ReviewDto getReviewById(Long reviewId) {
-        return reviewStorage.getReviewById(reviewId);
+        log.info("Получаем отзыв по ID: {}", reviewId);
+        return reviewMapper.toDto(jpaReviewRepository.getReviewById(reviewId));
     }
 
+    @Transactional
     public ReviewDto addReview(ReviewDto reviewDto) {
-        return reviewStorage.addReview(reviewDto);
+        log.info("Добавляем отзыв: {}", reviewDto);
+        Review review = reviewMapper.toEntity(reviewDto);
+        return reviewMapper.toDto(jpaReviewRepository.save(review));
     }
 
+    @Transactional
     public ReviewDto updateReview(ReviewDto reviewDto, Long reviewId) {
-        if(reviewStorage.getReviewById(reviewId).equals(null)) {
-            throw new BadRequestException(HttpStatus.BAD_REQUEST, "Такого отзыва не существует" + reviewId);
+        log.info("Обновляем отзыв с ID: {}", reviewId);
+        if(jpaReviewRepository.getReviewById(reviewId).equals(null)) {
+            log.error("Отзыва с ID {} не существует", reviewId);
+            throw new BadRequestException(HttpStatus.NOT_FOUND, "Такого отзыва не существует" + reviewId);
         }
-        return reviewStorage.updateReview(reviewDto);
+        Review review = reviewMapper.toEntity(reviewDto);
+        return reviewMapper.toDto(jpaReviewRepository.save(review));
     }
 
+    @Transactional
     public void deleteReview(Long reviewId) {
-        if(reviewStorage.getReviewById(reviewId).equals(null)) {
+        log.info("Удаляем отзыв с ID: {}", reviewId);
+        if(jpaReviewRepository.getReviewById(reviewId).equals(null)) {
+            log.error("Отзыва с ID {} не существует", reviewId);
             throw new BadRequestException(HttpStatus.BAD_REQUEST, "Такого отзыва не существует" + reviewId);
         } else {
-            reviewStorage.deleteReview(reviewId);
+            jpaReviewRepository.deleteReviewById(reviewId);
         }
     }
 
+    @Transactional
     public List<ReviewDto> getAllReviewsByUserId(Long userId) {
-        return reviewStorage.getAllReviewsByUserId(userId);
+        log.info("Получаем все отзывы пользователя с ID: {}", userId);
+        List<Review> reviewList = jpaReviewRepository.getAllReviewsByUserId(userId);
+        return reviewList.stream()
+                .map(reviewMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
