936bcade7b34ce4ab796b385b17dcaa4456c3f74
Olesia <olesia04@yandex.ru>
2024-07-13 15:30:40 +0400

    Добавлены юнит тесты слоя контроллера

--- certificate-service/src/main/java/com/org/example/CertificateServiceRunner.java
+++ certificate-service/src/main/java/com/org/example/CertificateServiceRunner.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CertificateServiceRunner {
     public static void main(String[] args) {
-        try {
             SpringApplication.run(CertificateServiceRunner.class, args);
-        } catch (Exception e){
-            System.out.printf("Не работает" + e.getMessage());
-        }
-
     }
 }
